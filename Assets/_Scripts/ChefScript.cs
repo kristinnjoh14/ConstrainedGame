54d03b210059b6a54771f583be1c4452d25898a8
Kiddi <kiddinn95@gmail.com>
2018-02-07 18:21:03 +0000

    Dont mess it up, ya git

---
+++ Assets/_Scripts/ChefScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChefScript : MonoBehaviour {
+	public float chefSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InputHandler ();
+	}
+
+	void InputHandler () {
+		if (Input.GetKey (KeyCode.A)) {
+			if (!Input.GetKey (KeyCode.D)) {
+				//GetComponent<Rigidbody2D> ().AddForce (new Vector2 (-chefSpeed, 0));
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (-chefSpeed, 0);
+			} else {
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+			}
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			if (!Input.GetKey (KeyCode.A)) {
+				//GetComponent<Rigidbody2D> ().AddForce (new Vector2 (chefSpeed, 0));
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (chefSpeed, 0);
+			} else {
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+			}
+		}
+		if (Input.GetKeyUp (KeyCode.A)) {
+			//GetComponent<Rigidbody2D> ().AddForce (new Vector2 (-chefSpeed, 0));
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+		}
+		if (Input.GetKeyUp (KeyCode.D)) {
+			//GetComponent<Rigidbody2D> ().AddForce (new Vector2 (chefSpeed, 0));
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D coll) {
+		Debug.Log (coll.otherCollider.name);
+	}
+}
+
+
---
+++ Assets/_Scripts/IngredientSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSpawner : MonoBehaviour {
+	public float spawnRate;
+	public Transform[] ingredients = new Transform[6];
+
+	private float spawnCountdown;
+	// Use this for initialization
+	void Start () {
+		spawnCountdown = spawnRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnCountdown -= Time.deltaTime;
+		if (spawnCountdown < 0) {
+			//Spawn random ingredient
+			int index = Random.Range (0, ingredients.Length);
+			Instantiate (ingredients[index], new Vector2 ((Random.value*17.2f-8.6f), 5), Quaternion.identity);
+			
+			//Reset counter, += to include what little time was overkill for counter to reach 0
+			spawnCountdown += spawnRate;
+		}
+	}
+}
