b6d5ca659a3f07905be7b84b098b56f1e2e4e575
Arnór Rafn Gíslason <arnorrg15@ru.is>
2018-02-12 02:00:24 +0000

    Main scene

--- Assets/_Scripts/Stack.cs
+++ Assets/_Scripts/Stack.cs
@@ -26,31 +26,18 @@ public class Stack : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (stack.Count > 0) {
-			//if (Mathf.Abs (GetComponentInParent<Rigidbody2D> ().transform.position.x - stack [0].transform.position.x) > 0.2) {
-			Vector2 chef = GetComponentInParent<Rigidbody2D> ().transform.position;
-			//Debug.Log (chef);
-			//Vector3 temp = stack [0].transform.position;
-			//float x = Mathf.LerpUnclamped (temp.x, chef.x, stackStiffness);
+
+            GameObject plate = GameObject.FindGameObjectWithTag("Plate");
+            Vector2 chef = plate.transform.position;
 			Vector3 v = new Vector3 (chef.x, stack[0].transform.position.y);
 			stack [0].transform.position = v;
-			//}
-			/*for (int i = stack.Count - 1; i > 0; i--) {
-				//if (Mathf.Abs (stack[i].transform.position.x - item.transform.position.x) > 0.2) {
-				Vector2 tmp1 = stack[i].transform.position;
-				Vector3 temp1 = stack[i-1].transform.position;
-				float x1 = Mathf.LerpUnclamped (tmp1.x, temp1.x, stackStiffness);
-				Vector3 v2 = new Vector3 (x1, tmp1.y);
-				stack[i].transform.position = v2;
-				//}
-			}*/
+
 			for (int i = 1; i < stack.Count; i++) {
-				//if (Mathf.Abs (stack[i].transform.position.x - item.transform.position.x) > 0.2) {
 				Vector2 tmp1 = stack[i].transform.position;
 				Vector3 temp1 = stack[i-1].transform.position;
 				float x1 = Mathf.LerpUnclamped (tmp1.x, temp1.x, stackStiffness);
 				Vector3 v2 = new Vector3 (x1, tmp1.y);
 				stack[i].transform.position = v2;
-				//}
 			}
 			GetComponent<BoxCollider2D> ().transform.position = new Vector2 (stack[stack.Count-1].transform.position.x, stackTop.y);
 		}
@@ -59,13 +46,14 @@ public class Stack : MonoBehaviour {
 	public void addToStack (int type) {
 		if (type == 1) {
 			foreach (GameObject item in stack) {
-				Destroy (item);
+                Destroy (item);
                 myScoreManager.addToScore(100);
 			}
 			stack.Clear ();
 			stackTop = (Vector2) transform.parent.position;
 		}
 		Transform newbie = Instantiate (ingredients[type], stackTop + new Vector2 (0,ingredientHeight), Quaternion.identity);
+	    newbie.GetComponent<SpriteRenderer>().sortingOrder = stack.Count;
 		stack.Add (newbie.gameObject);
 		stackTop = newbie.position;
 		GetComponent<BoxCollider2D> ().transform.position = new Vector2 (GetComponent<BoxCollider2D> ().transform.position.x, stackTop.y);
