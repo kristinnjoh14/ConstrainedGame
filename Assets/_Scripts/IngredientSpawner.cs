724b752e2ceb8a3b3f5ee4af65def3b28e8c4e09
Kiddi <kiddinn95@gmail.com>
2018-02-13 23:19:02 +0000

    hopefully trashcans are in view. Balcony made tight and of pants, ingredient spawner now spawns from balcony

--- Assets/_Scripts/IngredientSpawner.cs
+++ Assets/_Scripts/IngredientSpawner.cs
@@ -20,8 +20,8 @@ public class IngredientSpawner : MonoBehaviour {
 			int index = Random.Range (0, ingredients.Length-1);
 			Vector2 topLeft = Camera.main.ScreenToWorldPoint (new Vector3 (0, Screen.height*0.8f, 0));
 			Vector2 topRight = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height*0.8f, 0));
-			topLeft.x += 1024 / 600;
-			topRight.x -= 1024 / 600;
+			topLeft.x += (1024 / 600) + 4.1f;
+			topRight.x -= 1024 / 600 + 4.1f;
 			Instantiate (ingredients[index], new Vector2 (topLeft.x+2*Random.value*topRight.x,topLeft.y), Quaternion.identity);
 			
 			//Reset counter, += to include what little time was overkill for counter to reach 0
